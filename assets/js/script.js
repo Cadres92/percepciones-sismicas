7eb0456e75ec8c4590b02aa4a1952e1ae531f181
Vicente Aguirre <vicenteignacioac22@gmail.com>
2022-12-14 05:44:13 -0300

    fixed login function

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,7 +1,7 @@
 const validarDatos = () => {
     usuario = document.querySelector("#usuario-input").value
     contraseña = document.querySelector("#contraseña-input").value
-    if (usuario === "inacap" || "Inacap" && contraseña === "4310") {
+    if (usuario === "inacap", "Inacap" && contraseña === "4310") {
         window.location.assign("main.html")
     }
     else if (usuario === "camilo" && contraseña === "inacap") {
