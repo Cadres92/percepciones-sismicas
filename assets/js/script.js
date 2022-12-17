563e3258a699a8ad32099c0626a79fe2984be6db
Cadres92 <120241419+Cadres92@users.noreply.github.com>
2022-12-17 02:34:53 -0300

    Update script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,13 +1,13 @@
 const validarDatos = () => {
     usuario = document.querySelector("#usuario-input").value
     contraseña = document.querySelector("#contraseña-input").value
-    if (usuario === "inacap" && contraseña === "4310") {
+    if (usuario === "tirana" && contraseña === "Tir.2022") {
         window.location.assign("main.html")
     }
     else if (usuario === "camilo" && contraseña === "inacap") {
         window.location.assign("main.html")
     }
-    else if (usuario === "Inacap" && contraseña === "4310") {
+    else if (usuario === "Tirana" && contraseña === "Tir.2022") {
         window.location.assign("main.html")
     }
     else {
